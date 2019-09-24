39cb3620d2b525e4deef58f1c8913a3a8a5443b0
Julio Moreira B. Barros <julio.barros@movile.com>
2019-09-24 01:21:54 -0300

    get input and evolve distance functions

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,12 @@ module Main where
 -- :set -package containers
 import qualified Data.Map.Strict as StrictMap
 import Data.List
+import System.IO (isEOF)
 type Map = StrictMap.Map
 
 sq a = a * a
 
-data Point = Point [Char] [Double]
+data Point = Point [Char] [Double] deriving Show
 
 dist (Point _ p1) (Point _ p2) = sqrt (sum $ map (\(a, b) -> sq (a - b)) $ zip p1 p2)
 
@@ -25,13 +26,48 @@ createPointFromLine :: [Char] -> Point
 createPointFromLine line = Point (head tokens) (map read $ tail tokens)
     where tokens = splitStr line ' '
 
-attachLabel :: Point -> [Char] -> Map [Char] [Point] -> Map [Char] [Point]
-attachLabel point label mapa
+attachLabel :: Map [Char] [Point] -> [Char] -> Point -> Map [Char] [Point]
+attachLabel mapa label point
     | StrictMap.member label mapa = StrictMap.adjust (\v -> point:v) label mapa
     | otherwise = StrictMap.insert label [point] mapa
+
+
+getDist :: ([Char], [Point], Double, Point) -> Double
+getDist (_, _, d, _) = d
+
+simplify :: ([Char], [Point], Double, Point) -> ([Char], Point)
+simplify (lbl, _, _, p) = (lbl, p)
+
+minimumDistance :: [Point] -> Point -> Double
+minimumDistance [] _ = 0
+minimumDistance points p = dist (minimumBy (\p1 -> \p2 -> compare (dist p p1) (dist p p2)) points) p
+
+minimumTuple :: [([Char], [Point])] -> Point -> ([Char], [Point], Double) 
+minimumTuple pointTuples p = 
+    minimumBy (\(_, _, d1) -> \(_, _, d2) -> compare d1 d2) pointTupleDistances
+    where pointTupleDistances = map (\(lbl, ps1) -> (lbl, ps1, (minimumDistance ps1 p))) pointTuples
+
+getNearestLabel :: Map [Char] [Point] -> Point -> ([Char], [Point], Double)
+getNearestLabel mapa p = minimumTuple (StrictMap.assocs mapa) p
+
+getNearestPointAndLabel :: Map [Char] [Point] -> [Point] -> ([Char], Point)
+getNearestPointAndLabel mapa ps =
+    simplify $ minimumBy (\p1 -> \p2 -> compare (getDist p1) (getDist p2)) distances
+    where distances = map (\((a,b,c),d) -> (a,b,c,d)) $ map (\p -> ((getNearestLabel mapa p),p)) ps
+-- enrich :: Map [Char] [Point] -> [Point] -> Map [Char] [Point]
+
+
+getPointsFromInput :: [Point] -> IO ()
+getPointsFromInput pontos = do
+   end <- isEOF
+   if end then
+       print pontos
+   else do
+       linha <- getLine
+       getPointsFromInput ((createPointFromLine linha):pontos)
 
 main :: IO ()
 --main = print (dist (Point 3.0 4.0) (Point 4.0 3.0))
 --main = print $ splitStr "meu amigo feliz" ' '
 -- main = print $ dist (Point [3, 4, 5]) (Point [4, 3, 12])
-main = print "bla"
+main = getPointsFromInput []
--- out/production/proj1/Main.hs
+++ out/production/proj1/Main.hs
@@ -4,13 +4,14 @@ module Main where
 -- :set -package containers
 import qualified Data.Map.Strict as StrictMap
 import Data.List
+import System.IO (isEOF)
 type Map = StrictMap.Map
 
 sq a = a * a
 
-data Point = Point [Double]
+data Point = Point [Char] [Double] deriving Show
 
-dist (Point p1) (Point p2) = sqrt (sum $ map (\(a, b) -> sq (a - b)) $ zip p1 p2)
+dist (Point _ p1) (Point _ p2) = sqrt (sum $ map (\(a, b) -> sq (a - b)) $ zip p1 p2)
 
 splitStr list delimiter = splitStr' [] [] list delimiter
     where splitStr' :: Eq a => [[a]] -> [a] -> [a] -> a -> [[a]]
@@ -21,14 +22,52 @@ splitStr list delimiter = splitStr' [] [] list delimiter
             | x == delimiter = splitStr' finalAcc [] xs delimiter
             | otherwise = splitStr' finalAcc (acc++[x]) xs delimiter
 
-createTupleFromLine :: [Char] -> ([Char], Point)
-createTupleFromLine line = (head tokens, Point (map read $ tail tokens))
+createPointFromLine :: [Char] -> Point
+createPointFromLine line = Point (head tokens) (map read $ tail tokens)
     where tokens = splitStr line ' '
 
-attachLabel :: [Char] -> Map [Char] (Map [Char] Point) -> Map [Char] (Map [Char] Point) 
+attachLabel :: Map [Char] [Point] -> [Char] -> Point -> Map [Char] [Point]
+attachLabel mapa label point
+    | StrictMap.member label mapa = StrictMap.adjust (\v -> point:v) label mapa
+    | otherwise = StrictMap.insert label [point] mapa
+
+
+getDist :: ([Char], [Point], Double, Point) -> Double
+getDist (_, _, d, _) = d
+
+simplify :: ([Char], [Point], Double, Point) -> ([Char], Point)
+simplify (lbl, _, _, p) = (lbl, p)
+
+minimumDistance :: [Point] -> Point -> Double
+minimumDistance [] _ = 0
+minimumDistance points p = dist (minimumBy (\p1 -> \p2 -> compare (dist p p1) (dist p p2)) points) p
+
+minimumTuple :: [([Char], [Point])] -> Point -> ([Char], [Point], Double) 
+minimumTuple pointTuples p = 
+    minimumBy (\(_, _, d1) -> \(_, _, d2) -> compare d1 d2) pointTupleDistances
+    where pointTupleDistances = map (\(lbl, ps1) -> (lbl, ps1, (minimumDistance ps1 p))) pointTuples
+
+getNearestLabel :: Map [Char] [Point] -> Point -> ([Char], [Point], Double)
+getNearestLabel mapa p = minimumTuple (StrictMap.assocs mapa) p
+
+getNearestPointAndLabel :: Map [Char] [Point] -> [Point] -> ([Char], Point)
+getNearestPointAndLabel mapa ps =
+    simplify $ minimumBy (\p1 -> \p2 -> compare (getDist p1) (getDist p2)) distances
+    where distances = map (\((a,b,c),d) -> (a,b,c,d)) $ map (\p -> ((getNearestLabel mapa p),p)) ps
+-- enrich :: Map [Char] [Point] -> [Point] -> Map [Char] [Point]
+
+
+getPointsFromInput :: [Point] -> IO ()
+getPointsFromInput pontos = do
+   end <- isEOF
+   if end then
+       print pontos
+   else do
+       linha <- getLine
+       getPointsFromInput ((createPointFromLine linha):pontos)
 
 main :: IO ()
 --main = print (dist (Point 3.0 4.0) (Point 4.0 3.0))
 --main = print $ splitStr "meu amigo feliz" ' '
 -- main = print $ dist (Point [3, 4, 5]) (Point [4, 3, 12])
-main = print "bla"
+main = getPointsFromInput []
